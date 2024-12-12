c18a93e12235fcc9eccb111f5f0f2fc9f353587a
Jacob Stevens <jakestevens082@gmail.com>
2024-12-11 19:26:35 -0800

    update deploy config

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/sort-visualizer' : ''
+			base: '/sorts'
 		}
 	},
 	preprocess: vitePreprocess()
